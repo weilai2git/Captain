429a7b7ecf0996df13cca891d3d8ce1fa9d76b7e
lai.wei <lai.wei@ymm56.com>
2017-05-26 10:33:16 +0800

    [FIX] rocketMq

--- src/main/java/com/future/tech/captain/mq/rocketmq/MessageSenderRocketMQImpl.java
+++ src/main/java/com/future/tech/captain/mq/rocketmq/MessageSenderRocketMQImpl.java
@@ -4,10 +4,16 @@
 package com.future.tech.captain.mq.rocketmq;
 
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendCallback;
+import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageExt;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.future.tech.captain.domain.MessageWrapper;
+import com.future.tech.captain.domain.MessageWrapperIdentity;
 import com.future.tech.captain.mq.MessageSender;
+import com.future.tech.captain.repository.MessageRepository;
 
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
@@ -17,42 +23,59 @@ import lombok.extern.slf4j.Slf4j;
  * Title: MessageSenderRocketMQImpl.java<br>
  * Description: <br>
  * Copyright: Copyright (c) 2017<br>
- * Company:  FutureTech<br>
+ * Company: FutureTech<br>
  * 
- * @author weilai
- * May 23, 2017
+ * @author weilai May 23, 2017
  */
 
 @Slf4j
-public class MessageSenderRocketMQImpl implements MessageSender{
-	
+public class MessageSenderRocketMQImpl implements MessageSender {
+
 	@Autowired
 	private DefaultMQProducer producer;
-	
+
 	@Setter
 	private String topic;
-	
+
 	@Setter
 	private String tags;
-	
-	@Autowired
-	private MessageCallback messageCallback;
 
-	/* (non-Javadoc)
-	 * @see com.future.tech.captain.mq.MessageSender#send(java.lang.String, java.lang.Object)
+	@Autowired
+	private MessageRepository messageRepository;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.future.tech.captain.mq.MessageSender#send(java.lang.String,
+	 * java.lang.Object)
 	 */
 	@Override
-	public boolean send(String correlationId, Object message) {
-		Message rocketMsg = new Message(topic, tags, correlationId, (byte[])message);
+	public boolean send(final String correlationId, Object message) {
+		Message rocketMsg = new Message(topic, tags, correlationId, (byte[]) message);
 		try {
-			producer.send(rocketMsg, messageCallback);
+			producer.send(rocketMsg, new SendCallback() {
+				@Override
+				public void onSuccess(SendResult sendResult) {
+					MessageWrapperIdentity messageWrapperIdentity = new MessageWrapperIdentity(correlationId);
+					MessageWrapper messageWrapper = messageRepository.loadMessage(messageWrapperIdentity);
+					if (messageWrapper != null && messageWrapper.confirm()) {
+						messageRepository.store(messageWrapper);
+					}
+				}
+				@Override
+				public void onException(Throwable e) {
+					log.error(e.getMessage(), e);
+				}
+			});
 		} catch (Exception ex) {
 			log.error(ex.getMessage(), ex);
 		}
 		return true;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.future.tech.captain.mq.MessageSender#isSynConfirm()
 	 */
 	@Override
